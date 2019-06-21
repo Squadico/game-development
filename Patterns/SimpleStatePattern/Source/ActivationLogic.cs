775279107bcf2ea64c743080af8233120fddea07
Squadico <Squadico@users.noreply.github.com>
2019-06-21 14:04:16 +0200

    Initial version of Simple state pattern

---
+++ Patterns/SimpleStatePattern/Source/ActivationLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tests")]
+namespace SimpleStatePattern
+{
+    public class ActivationLogic
+    {
+        public event Action Enabled;
+        public event Action Disabled;
+
+        State m_State;
+
+        public ActivationLogic()
+        {
+            m_State = State.Disabled;
+        }
+        public void Enable()
+        {
+            m_State.EnableEvent(this);
+        }
+        public void Disable()
+        {
+            m_State.DisableEvent(this);
+        }
+
+        internal void SetState(State state)
+        {
+            m_State = state;
+        }
+
+        internal void EnableAction()
+        {
+            Enabled?.Invoke();
+        }
+        internal void DisableAction()
+        {
+            Disabled?.Invoke();
+        }
+    }
+}
---
+++ Patterns/SimpleStatePattern/Tests/ActivationLogicTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using SimpleStatePattern;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ActivationLogicTest
+    {
+        string EventRaised;
+        ActivationLogic Logic;
+
+        [SetUp]
+        public void Setup()
+        {
+            EventRaised = string.Empty;
+            Logic = new ActivationLogic();
+
+            Logic.Enabled += () => EventRaised += "Enabled";
+            Logic.Disabled += () => EventRaised += "Disabled";
+        }
+
+        [Test]
+        public void DisabledState_EnableEvent_EnableActionCalled()
+        {
+            Logic.Enable();
+            Assert.AreEqual("Enabled", EventRaised);
+        }
+
+        [Test]
+        public void DisabledState_DisableEvent_NothingCalled()
+        {
+            Logic.Disable();
+            Assert.AreEqual(string.Empty, EventRaised);
+        }
+
+        [Test]
+        public void EnabledState_DisableEvent_DisableActionCalled()
+        {
+            Logic.SetState(State.Enabled);
+            Logic.Disable();
+            Assert.AreEqual("Disabled", EventRaised);
+        }
+
+        [Test]
+        public void EnabledState_EnableEvent_NothingCalled()
+        {
+            Logic.SetState(State.Enabled);
+            Logic.Enable();
+            Assert.AreEqual(string.Empty, EventRaised);
+        }
+
+        [Test]
+        public void DisabledState_FullPathToDisabledStateTwice_CallsEnableAndDisableInTurns()
+        {
+            Logic.Enable();
+            Logic.Disable();
+            Logic.Enable();
+            Logic.Disable();
+
+            Assert.AreEqual("EnabledDisabledEnabledDisabled", EventRaised);
+        }
+    }
+}
