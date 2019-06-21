ef4e627b417d883b79f48d3216528e8101eabae6
Squadico <Squadico@users.noreply.github.com>
2019-06-21 16:29:42 +0200

    Simple State Pattern usage example is added

---
+++ Patterns/UsageExample/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace UsageExamples
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SimpleStatePatternExampleButton_Click(object sender, System.EventArgs e)
+        {
+            var example = new SimpleStatePatternExampleForm(this);
+            example.Show();
+        }
+    }
+}
---
+++ Patterns/UsageExample/SimpleStatePatternExampleForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UsageExamples
+{
+    partial class SimpleStatePatternExampleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Log = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.EnableButton = new System.Windows.Forms.Button();
+            this.DisableButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Log
+            // 
+            this.Log.BackColor = System.Drawing.SystemColors.Control;
+            this.Log.Location = new System.Drawing.Point(6, 19);
+            this.Log.Name = "Log";
+            this.Log.ReadOnly = true;
+            this.Log.Size = new System.Drawing.Size(174, 229);
+            this.Log.TabIndex = 0;
+            this.Log.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DisableButton);
+            this.groupBox1.Controls.Add(this.EnableButton);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 140);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Methods";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Log);
+            this.groupBox3.Location = new System.Drawing.Point(13, 159);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(189, 260);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Log";
+            // 
+            // EnableButton
+            // 
+            this.EnableButton.Location = new System.Drawing.Point(9, 28);
+            this.EnableButton.Name = "EnableButton";
+            this.EnableButton.Size = new System.Drawing.Size(171, 37);
+            this.EnableButton.TabIndex = 0;
+            this.EnableButton.Text = "Enable";
+            this.EnableButton.UseVisualStyleBackColor = true;
+            this.EnableButton.Click += new System.EventHandler(this.EnableButton_Click);
+            // 
+            // DisableButton
+            // 
+            this.DisableButton.Location = new System.Drawing.Point(9, 77);
+            this.DisableButton.Name = "DisableButton";
+            this.DisableButton.Size = new System.Drawing.Size(171, 41);
+            this.DisableButton.TabIndex = 1;
+            this.DisableButton.Text = "Disable";
+            this.DisableButton.UseVisualStyleBackColor = true;
+            this.DisableButton.Click += new System.EventHandler(this.DisableButton_Click);
+            // 
+            // SimpleStatePatternExampleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 428);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "SimpleStatePatternExampleForm";
+            this.Text = "SimpleStatePatternExampleForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SimpleStatePatternExampleForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Log;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button DisableButton;
+        private System.Windows.Forms.Button EnableButton;
+    }
+}
