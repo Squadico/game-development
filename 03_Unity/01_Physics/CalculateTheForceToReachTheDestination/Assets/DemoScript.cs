c3f29860ea00863029133f952cf143620c2b4c9c
Squadico <squadico@gmail.com>
2019-07-08 10:40:55 +0200

    Added first solution for Unity , Physics related

---
+++ 03_Unity/01_Physics/CalculateTheForceToReachTheDestination/Assets/DemoScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+    [SerializeField] GameObject Target;
+    [SerializeField] GameObject Projectile;
+    
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        Utility.Throw(Projectile, Target.GetComponent<Transform>().position);
+    }
+}
---
+++ 03_Unity/01_Physics/CalculateTheForceToReachTheDestination/Assets/Utility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Utility
+{
+    public static void Throw(GameObject obj, Vector3 destination)
+    {
+        var body = obj.GetComponent<Rigidbody>();
+        var position = obj.transform.position;
+        float distance = Vector3.Distance(position, destination);
+        Vector3 direction = (destination - position).normalized;
+        
+        //We set angle to 45 degrees that eliminates Sin from the formula
+        direction.y = 0.5f;         
+        
+        var force = Mathf.Sqrt(distance * Mathf.Abs(Physics.gravity.y)) * direction * body.mass;
+  
+        body.AddForce(force, ForceMode.Impulse); 
+    }
+}
