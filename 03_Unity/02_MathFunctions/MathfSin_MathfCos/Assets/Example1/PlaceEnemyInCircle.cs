a29f0849a762b929109068b2bd1b7a72f7916598
Squadico <squadico@gmail.com>
2019-07-10 11:00:32 +0200

    First Unity, Math exmple is added (Mathf.Cos/sin functions are covered)

---
+++ 03_Unity/02_MathFunctions/MathfSin_MathfCos/Assets/Example1/PlaceEnemyInCircle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlaceEnemyInCircle : MonoBehaviour
+{
+    public Object EnemyPrefab;
+    //Distance from Enemy to Player
+    public float Distance = 4;
+    //Angle around Player that Enemy can be placed
+    public float Angle = 360;
+    //Enemy's count  
+    public int count = 15;
+
+    void Start ()
+    {
+        Vector3 position = transform.position;
+        Angle = Angle * Mathf.Deg2Rad;
+        for(int i = 1; i <= count; i++)
+        {
+            float _z = transform.position.z + Mathf.Cos(Angle/count*i)*Distance;
+            float _x = transform.position.x + Mathf.Sin(Angle/count*i)*Distance;
+            position.x = _x;
+            position.z = _z;
+            Instantiate(EnemyPrefab, position, Quaternion.identity);
+        }
+    }
+}
