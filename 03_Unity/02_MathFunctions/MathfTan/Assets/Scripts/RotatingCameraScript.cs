cea03c946b74be24bbc92d0b7ae27b8c7e226c83
Squadico <squadico@gmail.com>
2019-07-13 10:48:44 +0200

    Added Unity Mathf.Tan example

---
+++ 03_Unity/02_MathFunctions/MathfTan/Assets/Scripts/RotatingCameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class RotatingCameraScript : MonoBehaviour
+{
+    bool enemyDetected;
+    TrackingScript TrackingScript;
+
+    void Awake()
+    {
+        TrackingScript = GetComponentInChildren<TrackingScript>();
+        TrackingScript.EnemyDetected += () => enemyDetected = true;
+    }
+
+    void Update()
+    {
+        if (!enemyDetected)
+            transform.Rotate(0, 50 * Time.deltaTime, 0);
+    }
+}
---
+++ 03_Unity/02_MathFunctions/MathfTan/Assets/Scripts/TrackingScript.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class TrackingScript : MonoBehaviour
+{
+    public Action EnemyDetected;
+    
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        //Can use layer mask to filter detected objects 
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 4.25f))
+        {
+            //More filtering can be added here, e.g. checking tags , etc.  
+            
+            float angle = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+            if(Mathf.Cos(angle) > 0)
+            {
+                if(Mathf.Tan(angle) < 0)
+                {
+                    Debug.Log("Zone 1");
+                }
+                else
+                {
+                    Debug.Log("Zone 2");
+                }
+            }
+            else                                
+            {
+                if(Mathf.Tan(angle) < 0)     
+                {
+                    Debug.Log("Zone 3");
+                }
+                else
+                {
+                    Debug.Log("Zone 4");
+                }
+            }
+            
+            //Notifying camera above to stop rotation (in our particular case)
+            EnemyDetected?.Invoke();
+        }
+        Debug.DrawRay(transform.position, transform.forward * 4.25f, Color.red, 0.05f); 
+    }
+}
