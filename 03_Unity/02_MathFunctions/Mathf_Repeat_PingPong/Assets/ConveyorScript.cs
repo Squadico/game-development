851113c77f8ada6f17c8bdd3f66228ea48663b04
Squadico <squadico@gmail.com>
2019-07-17 15:29:06 +0200

    Added Mathf.PingPong and Mathf.Repeat functions examples

---
+++ 03_Unity/02_MathFunctions/Mathf_Repeat_PingPong/Assets/ConveyorScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ConveyorScript : MonoBehaviour
+{
+    int distance = 5;
+    
+    void Update ()
+    {
+        transform.position = new Vector3(Mathf.Repeat(Time.time, distance), transform.position.y, transform.position.z);
+    }
+}
---
+++ 03_Unity/02_MathFunctions/Mathf_Repeat_PingPong/Assets/PatrolScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PatrolScript : MonoBehaviour
+{
+    float speed = 1;
+    float distance = 5;
+    void Update ()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, distance), transform.position.y, transform.position.z);
+    }
+}
